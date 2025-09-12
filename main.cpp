aabeeb12b23160d745b71c68f066a6e1a0036b2d
Nikita Jegorov <proxjega@gmail.com>
2025-09-13 00:39:48 +0300

    HashFunction returns string

--- main.cpp
+++ main.cpp
@@ -14,16 +14,16 @@
 
 using namespace std; 
 
-int HashFunction(string input){
+string HashFunction(string input){
     constexpr int HASH_LENGTH = 64;
     int hash[HASH_LENGTH];
-    vector<int> bitVector;
-    bitVector.reserve(input.length()*2);
+    vector<int> integerVector;
+    integerVector.reserve(input.length()*2);
     long int uniqueStringNumber = 1;
     int counter = 0;
     for (char c : input) {
         int castedChar = static_cast<int>(c);
-        bitVector.push_back(castedChar);
+        integerVector.push_back(castedChar);
         if (counter % 4 == 0) uniqueStringNumber = abs(uniqueStringNumber) - 255;
         if (counter % 3 == 0) uniqueStringNumber = (uniqueStringNumber+1) * -1;
         if (counter % 2 == 0) uniqueStringNumber+=castedChar+1;
@@ -36,17 +36,22 @@ int HashFunction(string input){
     mt19937 engine(uniqueStringNumber+3);
     uniform_int_distribution<int> indexDistribution(0, HASH_LENGTH - 1);
     uniform_int_distribution<int> randomDistribution(0, 15);
-    for (auto el : bitVector) {
+    for (auto el : integerVector) {
         hash[indexDistribution(engine)]+= el;
     }
-
     for (int i = 0; i < HASH_LENGTH; i++) {
         hash[i]+= randomDistribution(engine);
         hash[i]=hash[i]%16;
-        cout << hex << hash[i];
     }
-    cout << endl;
-    return 0;
+
+    // convert to string
+    ostringstream oss;
+    for (int i = 0; i < 64; i++)
+        oss << std::hex << hash[i];
+
+    string hexstr = oss.str();
+    cout << hexstr << endl;
+    return hexstr;
 }
 
 int main(int argc, char** argv) {
