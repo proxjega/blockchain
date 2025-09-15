68d25b6112d93051c60e61618df55762c473bdeb
Nikita Jegorov <proxjega@gmail.com>
2025-09-15 18:49:05 +0300

    konstitucija.txt check

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <bitset>
 #include <cstddef>
+#include <cstdio>
 #include <cstdlib>
 #include <fstream>
 #include <functional>
@@ -10,6 +11,7 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <chrono>
 #include <random>
 #include <cmath>
 #include <iomanip>
@@ -128,11 +130,40 @@ void CheckHashesForCollision(string resultFileName){
         }
     }
 }
-//to do: switch case for argument handling, fix checkHashes for collision;
+
+void CheckKonstitucija(){
+    ifstream file("konstitucija.txt");
+    if (!file){
+        cout << "File not found!\n";
+        return;
+    }
+    string text;
+    ofstream resultFile("konstitucijaResults.txt");
+    for (int i = 1; i < 789; i*=2){
+        text.clear();
+        file.close();
+        file.open("konstitucija.txt");
+        for (int j = 0; j < i; j++) {
+            string line;
+            getline(file, line);
+            text += line;
+        }
+        double avgTime;
+        auto start = chrono::high_resolution_clock::now();
+        for (int k = 0; k < 5; k++) {
+            string hash = HashFunction(text);
+        }
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<double, std::milli> diff = end - start;
+        avgTime = diff.count() / 5.0;
+        resultFile << "Average time for hashing " << i << " lines: " << avgTime << "ms\n";
+    }
+}
 int main(int argc, char** argv) {
     // argument handling
     if (argc < 2) {
         cout << "Specify the option or file to Hash!\n";
+        cout << "-t - hash all files from /testcases\n -o - generate one char test files\n -r - generate 1000 random string files\n -d - generate random string files but with one different char\n -c - check resultHash.txt for collisions\n";
         return 0;
     }
     char option;
@@ -164,6 +195,13 @@ int main(int argc, char** argv) {
             cout << "Checking result file for collisions...\n";
             CheckHashesForCollision("hashResults.txt");
             return 0;
+            break;
+        case 'k':
+            cout << "Checking konstitucija.txt file...\n";
+            CheckKonstitucija();
+            return 0;
+            break;
+
     }
     if (!filesystem::exists(argv[1])) {
         cout << "File \"" << argv[1] << "\" does not exists!\n";
