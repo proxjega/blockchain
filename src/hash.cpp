c8eb999658d9f7facd9f1d03faa68c174290c1e2
Nikita Jegorov <proxjega@gmail.com>
2025-09-18 21:43:20 +0300

    salt

--- src/hash.cpp
+++ src/hash.cpp
@@ -1,7 +1,60 @@
 // hashfunction
 #include "../include/hash.h"
+#include <random>
 
 string HashFunction(const string &input){
+    constexpr int HASH_LENGTH = 64;
+    vector<int> integerVector;
+    integerVector.reserve(input.length()*2);
+    long int uniqueStringNumber = 1;
+    int counter = 0;
+    for (char c : input) {
+        int castedChar = static_cast<int>(c);
+        if (castedChar < 0) castedChar*= -1;
+        integerVector.push_back(castedChar);
+        if (counter % 4 == 0) uniqueStringNumber = std::abs(uniqueStringNumber) - 255;
+        if (counter % 3 == 0) uniqueStringNumber = (uniqueStringNumber+1) * -1;
+        if (counter % 2 == 0) uniqueStringNumber+=castedChar+1;
+        else uniqueStringNumber-=castedChar;
+        // cout << "castedchar " << castedChar << endl;
+        // cout << "USN " <<uniqueStringNumber << endl;
+        counter++;
+    }
+    // cout << "USN: " << uniqueStringNumber << endl;
+    mt19937 engine(uniqueStringNumber+3);
+    uniform_int_distribution<int> indexDistribution(0, HASH_LENGTH - 1);
+    uniform_int_distribution<int> randomDistribution(0, 15);
+    int hash[HASH_LENGTH];
+    for (int i = 0; i < HASH_LENGTH; i++) {
+        hash[i]=0;
+    }
+    for (auto el : integerVector) {
+        hash[indexDistribution(engine)]+= el;
+    }
+    for (int i = 0; i < HASH_LENGTH; i++) {
+        hash[i]+= randomDistribution(engine);
+        hash[i]=hash[i]%16;
+    }
+    // convert to string
+    ostringstream oss;
+    for (int i = 0; i < HASH_LENGTH; i++) {
+        oss << std::hex << hash[i];
+    }
+    string hexstr = oss.str();
+    return hexstr;
+}
+
+string HashFunction(string input, bool salt = false){
+    if (salt == true) {
+        std::random_device rd;
+        mt19937 engine(rd());
+        uniform_int_distribution<int> charDistribution(33, 126);
+        string s;
+        for (int i = 0; i < 10; i++) {
+            s+=static_cast<char>(charDistribution(engine));
+        }
+        input.append(s);
+    }
     constexpr int HASH_LENGTH = 64;
     vector<int> integerVector;
     integerVector.reserve(input.length()*2);
